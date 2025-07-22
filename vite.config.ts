c1a4aafdbf4414ce4fddff929626c9fb3120c17b
themixednuts <jonfonts@gmail.com>
2025-07-22 01:53:02 -0500

    change

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,27 @@ import { defineConfig } from "vite";
 import dts from "unplugin-dts/vite";
 
 export default defineConfig({
-  plugins: [dts()],
-  build: {
-    lib: {
-      entry: {
-        index: "src/index.ts",
-        "buffers/index": "src/buffers/index.ts", // Add this
-      },
-      formats: ["es"],
-    },
-    sourcemap: true,
-    rollupOptions: {
-      external: ["@bufbuild/protobuf"],
-      output: {
-        preserveModules: true, // Key setting!
-        preserveModulesRoot: "src",
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      "@": new URL("./src", import.meta.url).pathname,
-    },
-  },
+	plugins: [dts()],
+	build: {
+		lib: {
+			entry: {
+				index: "src/index.ts",
+				"buffers/index": "src/buffers/index.ts", // Add this
+			},
+			formats: ["es"],
+		},
+		sourcemap: true,
+		rollupOptions: {
+			external: ["@bufbuild/protobuf"],
+			output: {
+				preserveModules: true, // Key setting!
+				preserveModulesRoot: "src",
+			},
+		},
+	},
+	resolve: {
+		alias: {
+			"@": new URL("./src", import.meta.url).pathname,
+		},
+	},
 });
